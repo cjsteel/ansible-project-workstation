0eb9940e173b52fe4c2c4b6bcd30f9793b6ad402
Christopher Steel <chris.steel@gmail.com>
2019-02-25 20:47:38 -0500

    ansible project test outline

---
+++ init_ubuntu.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+# exit if a command fails
+set -e
+
+REPO=${ANSIBLE_PROJECT_REMOTE_REPO}
+CLONE_DIR=${ANSIBLE_PROJECT_PARENT_DIR}/${ANSIBLE_PROJECT_LOCAL_REPO}
+
+# install initial packages
+
+apt update
+apt install -y openssh-server
+apt install -y git
+apt install -y python-minimal
+apt install -y python-pip
+
+# clone this repo
+mkdir -p "${ANSIBLE_PROJECT_PARENT_DIR}"
+cd "${ANSIBLE_PROJECT_PARENT_DIR}"
+git clone --recursive "${ANSIBLE_PROJECT_REMOTE_REPO}" "${ANSIBLE_PROJECT_LOCAL_REPO}"
+
+# ensure for ansible
+git clone https://github.com/ansible/ansible.git --recursive $ cd ./ansible. ...
+source ./hacking/env-setup. ...
+source ./hacking/env-setup.fish. ...
+source ./hacking/env-setup -q. ...
+sudo easy_install pip. ...
+sudo pip install -r ./requirements.txt. ...
+git pull --rebase.
+
+#ansible-galaxy install -r requirements.yml
+#ANSIBLE_NOCOWS=1 ansible-playbook -i "${ANSIBLE_PROJECT_LOCAL_REPO}"/inventory/localhost "${ANSIBLE_PROJECT_LOCAL_REPO}"/site.yml
+
